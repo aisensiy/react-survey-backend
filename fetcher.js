f0b9edc47f0fdb2b8d3b71246e894f4bf0e14dc7
aisensiy <aisensiy@163.com>
2016-09-29 01:25:37 +0800

    add fetcher

---
+++ fetcher.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const fetcher = axios.create({
+  baseURL: 'https://api.leancloud.cn/1.1',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-LC-Id': process.env.APP_ID,
+    'X-LC-Key': process.env.MASTER_KEY + ',master'
+  }
+});
+
+export default fetcher;
